9573640b63acbdc86c7a70b779d8c203e43daa5b
SuttiGuy <Guy.guy0205245@gmail.com>
2024-03-02 22:22:26 +0700

    update_ProductList

--- client/src/pages/PrivateRouter/PrivateRouter.jsx
+++ client/src/pages/PrivateRouter/PrivateRouter.jsx
@@ -1,10 +1,39 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from '../../context/AuthProvider'
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const PrivateRouter = ({children}) =>{
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const location = useLocation();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const waitBeforeRedirect = setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+    
+        return () => clearTimeout(waitBeforeRedirect);
+      }, []);
+
+      useEffect(() => {
+        if (user === null) {
+          // user is still loading or not logged in
+          console.log("Waiting for user...");
+        } else if (user !== null) {
+          // user is logged in
+          console.log("User is logged in:", user);
+        } else {
+          // user is not logged in
+          console.log("Redirecting to /signIn");
+          // You can navigate or take other actions here
+        }
+      }, [user]);
+    
+      if (loading) {
+        // Show loading spinner or any other loading UI
+        return <div className="loading-animation">Loading...</div>;
+      }
+
     if (user) {
         return children;
     }
